dd4ad69babafbe5e73e05a248393fc8415de3d0d
Cho Kyung Hyun <jkh9615@naver.com>
2020-12-29 23:06:59 +0900

    Feat: Implement getting stock info
    <description>
    주식 정보 불러오는 기능 추가
    
    <issue-number>
    #5

--- ASTC-Client/frmMain.Designer.cs
+++ ASTC-Client/frmMain.Designer.cs
@@ -31,39 +31,73 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
 			this.axKHOpenAPI = new AxKHOpenAPILib.AxKHOpenAPI();
 			this.btnLoginTest = new System.Windows.Forms.Button();
+			this.btnSearch = new System.Windows.Forms.Button();
+			this.txtCode = new System.Windows.Forms.TextBox();
+			this.lblCode = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// axKHOpenAPI
 			// 
 			this.axKHOpenAPI.Enabled = true;
-			this.axKHOpenAPI.Location = new System.Drawing.Point(92, 38);
+			this.axKHOpenAPI.Location = new System.Drawing.Point(12, 388);
 			this.axKHOpenAPI.Name = "axKHOpenAPI";
 			this.axKHOpenAPI.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axKHOpenAPI.OcxState")));
 			this.axKHOpenAPI.Size = new System.Drawing.Size(100, 50);
 			this.axKHOpenAPI.TabIndex = 0;
+			this.axKHOpenAPI.Visible = false;
 			// 
 			// btnLoginTest
 			// 
-			this.btnLoginTest.Location = new System.Drawing.Point(92, 94);
+			this.btnLoginTest.Location = new System.Drawing.Point(12, 12);
 			this.btnLoginTest.Name = "btnLoginTest";
-			this.btnLoginTest.Size = new System.Drawing.Size(147, 47);
+			this.btnLoginTest.Size = new System.Drawing.Size(231, 47);
 			this.btnLoginTest.TabIndex = 1;
 			this.btnLoginTest.Text = "로그인 테스트";
 			this.btnLoginTest.UseVisualStyleBackColor = true;
 			this.btnLoginTest.Click += new System.EventHandler(this.BtnLoginTest_Click);
 			// 
-			// Form1
+			// btnSearch
+			// 
+			this.btnSearch.Location = new System.Drawing.Point(177, 65);
+			this.btnSearch.Name = "btnSearch";
+			this.btnSearch.Size = new System.Drawing.Size(66, 21);
+			this.btnSearch.TabIndex = 2;
+			this.btnSearch.Text = "정보 GET";
+			this.btnSearch.UseVisualStyleBackColor = true;
+			this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+			// 
+			// txtCode
+			// 
+			this.txtCode.Location = new System.Drawing.Point(83, 65);
+			this.txtCode.Name = "txtCode";
+			this.txtCode.Size = new System.Drawing.Size(88, 21);
+			this.txtCode.TabIndex = 3;
+			// 
+			// lblCode
+			// 
+			this.lblCode.AutoSize = true;
+			this.lblCode.Location = new System.Drawing.Point(12, 72);
+			this.lblCode.Name = "lblCode";
+			this.lblCode.Size = new System.Drawing.Size(65, 12);
+			this.lblCode.TabIndex = 4;
+			this.lblCode.Text = "종목 코드 :";
+			// 
+			// frmMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblCode);
+			this.Controls.Add(this.txtCode);
+			this.Controls.Add(this.btnSearch);
 			this.Controls.Add(this.btnLoginTest);
 			this.Controls.Add(this.axKHOpenAPI);
-			this.Name = "Form1";
+			this.Name = "frmMain";
 			this.Text = "ASTS-Client";
 			((System.ComponentModel.ISupportInitialize)(this.axKHOpenAPI)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -71,6 +105,9 @@
 
 		private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI;
 		private System.Windows.Forms.Button btnLoginTest;
+		private System.Windows.Forms.Button btnSearch;
+		private System.Windows.Forms.TextBox txtCode;
+		private System.Windows.Forms.Label lblCode;
 	}
 }
 
--- ASTC-Client/frmMain.cs
+++ ASTC-Client/frmMain.cs
@@ -16,6 +16,7 @@ namespace ASTC_Client
 		public frmMain()
 		{
 			InitializeComponent();
+			axKHOpenAPI.OnReceiveTrData += OnReceiveTrData; // 3. EventListener 등록
 		}
 
 		private void BtnLoginTest_Click(object sender, EventArgs e)
@@ -27,6 +28,31 @@ namespace ASTC_Client
 			else
 			{
 				MessageBox.Show("로그인창 열기 실패");
+			}
+		}
+
+		private void OnReceiveTrData(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnReceiveTrDataEvent e)
+		{
+			int receiveCnt = axKHOpenAPI.GetRepeatCnt(e.sTrCode, e.sRQName);
+			if ("주식기본정보".Equals(e.sRQName))
+			{
+				for (int i = 0; i <= receiveCnt; i++)
+				{
+					string jName = axKHOpenAPI.GetCommData(e.sTrCode, e.sRQName, i, "종목명"); // 4. GetCommData 메소드를 통해 데이터 접근
+					string jPrice = axKHOpenAPI.GetCommData(e.sTrCode, e.sRQName, i, "현재가"); // 4. GetCommData 메소드를 통해 데이터 접근
+					jName = jName.Trim();
+					jPrice = jPrice.Trim();
+					MessageBox.Show("[" + jName + "]\n현재가 : " + jPrice);
+				}
+			}
+		}
+		private void BtnSearch_Click(object sender, EventArgs e)
+		{
+			axKHOpenAPI.SetInputValue("종목코드", txtCode.Text); // 1. SetInputValue
+			int ret = axKHOpenAPI.CommRqData("주식기본정보", "OPT10001", 0, "1002"); // 2. CommRqData 를 통해 데이터 요청
+			if (ret < 0)
+			{
+				MessageBox.Show("요청 실패\nError Code : " + ret, "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 		}
 	}
